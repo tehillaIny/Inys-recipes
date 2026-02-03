4186705e4757382a3145c553087ebbb99494340f
Tehilla Iny <Tehilla.iny@gmail.com>
2026-02-03 19:03:32 +0200

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ התיקון החכם:
-  // אם אנחנו ב-Vercel (יש משתנה סביבה כזה) -> תהיה שרת רגיל (undefined) כדי שה-API יעבוד.
-  // אחרת (במחשב שלך כשאת בונה לאנדרואיד) -> תעשה export לקבצים סטטיים.
   output: process.env.VERCEL ? undefined : 'export',
 
   images: {
