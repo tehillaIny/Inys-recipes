97296e087fc55ed392c4497ba99ac4da1f08d3e6
Tehilla Iny <Tehilla.iny@gmail.com>
2026-06-14 16:14:24 +0300

    cooking mode - don't turn off the screen

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   output: process.env.VERCEL ? undefined : 'export',
@@ -13,4 +14,9 @@ const nextConfig: NextConfig = {
 
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public', 
+  disable: process.env.NODE_ENV === 'development', 
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
